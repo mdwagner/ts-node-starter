31e3838261dee6ad9eebcd1ec43f0e679304b2a7
Michael Wagner <mdwranger@gmail.com>
2019-06-30 19:28:11 -0400

    starter init

---
+++ __tests__/index.ts
@@ -0,0 +1,6 @@
+describe('index.ts', () => {
+  it('should work', () => {
+    const x: string = 'X';
+    expect(x).toBe('X');
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as path from 'path';
+
+const exactPath: string = path.resolve(__dirname, __filename);
+
+console.log(`Exact Path: ${exactPath}`);
